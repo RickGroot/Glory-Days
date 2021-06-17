e7cf3edb8984d4b2000a8d4be28cd25bf2a94156
Rick Groot <rickgroot41@gmail.com>
2021-06-17 12:13:02 +0200

    media query fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------------------------------------- Cached assets and version
-const cacheName = 'v2.2';
+const cacheName = 'v2.2.1';
 
 const cacheAssets = [
 	'css/global.css',
